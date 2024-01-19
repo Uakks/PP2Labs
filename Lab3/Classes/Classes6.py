97ad962039b6a32df53c02aceed1acd502a40206
Uakks <sopygaliu@mail.ru>
2024-01-19 22:45:58 +0600

    .

--- Lab3/Classes/Classes6.py
+++ Lab3/Classes/Classes6.py
@@ -1,2 +1,2 @@
 lst = [12, 13, 15, 16, 17, 18, 19, 20]
-print(list(filter(lambda x: True if x % (for i in range[2:x]) == 0 else False, lst)))
+print(filter(lambda x: True if x % (for i in range(2, x)) == 0 else False, lst))
---
+++ Lab3/functions/functions1-14.py
@@ -0,0 +1,3 @@
+from functions1 import Guess_Number
+
+Guess_Number(1,20)
