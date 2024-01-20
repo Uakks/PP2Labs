9cc8c6999fffeca7dd01e03dd0ae485d1cde07cd
Uakks <sopygaliu@mail.ru>
2024-01-20 09:39:12 +0600

    .

--- Lab3/Classes/Classes6.py
+++ Lab3/Classes/Classes6.py
@@ -1,4 +1,2 @@
-lst = [12, 13, 15, 16, 17, 18, 19, 20]
-print(filter(lambda x: True if x % (for i in range(2, x)) == 0 else False, lst))
-
-needed
+lst = [1, 2, 3, 4, 5, 12, 13, 15, 16, 17, 18, 19, 20]
+print(list(filter(lambda x: all(x % i != 0 for i in range(2, int(x))) and x != 1, lst)))
