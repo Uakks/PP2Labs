479b7ec3c4a667fea89f8a615424f93f67c8655f
Uakks <sopygaliu@mail.ru>
2024-01-26 00:48:47 +0600

    .

--- Lab7/Tutorials/pygame3tutorial.py
+++ Lab7/Tutorials/pygame3tutorial.py
@@ -5,11 +5,11 @@ screen = pygame.display.set_mode((400, 300))
 done = False
 play = True
 
-pygame.mixer.music.load('/Users/uakks/Desktop/Emin.mp3')
-pygame.mixer.music.queue('/Users/uakks/Music/Music/Media.localized/Apple Music/Eminem/The Eminem Show/10 Without Me.m4p')
+pygame.mixer.music.load('/Users/uakks/Desktop/Music_em/Emin.mp3')
 pygame.mixer.music.play(0)
 
 while not done:
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             play = not play
